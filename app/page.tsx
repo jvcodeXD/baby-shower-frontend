8ea1f7dace6f056273dc02fd57b8374b753581a4
Juan Visney <jvcode23@gmail.com>
2025-03-06 17:53:31 -0400

    correccion

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,44 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <Container maxWidth="sm">
-      <Paper
-        elevation={3}
-        sx={{ p: 6, mt: 4, textAlign: "center", bgcolor: "#ffe6f2" }}
-      >
-        <Typography variant="h3" fontWeight="bold" color="primary" gutterBottom>
-          🎀 ¡Bienvenido al Baby Shower! 🎀
-        </Typography>
-        <Typography variant="h6" color="textSecondary" gutterBottom>
-          Estamos muy emocionados de celebrar la llegada de nuestra pequeña
-          princesa. 💖
-        </Typography>
-
-        <Box mt={4}>
-          <Button
-            variant="contained"
+    <>
+      <Container maxWidth="md">
+        <Paper
+          elevation={3}
+          sx={{ p: 6, mt: 4, textAlign: "center", bgcolor: "#ffe6f2" }}
+        >
+          <Typography
+            variant="h3"
+            fontWeight="bold"
             color="primary"
-            sx={{ mr: 2 }}
-            onClick={() => router.push("/admin")}
+            gutterBottom
           >
-            Administrar Invitaciones
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            onClick={() => alert("¡Gracias por tu visita!")}
-          >
-            Más Información
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
+            🎀 ¡Bienvenido al Baby Shower! 🎀
+          </Typography>
+          <Typography variant="h6" color="textSecondary" gutterBottom>
+            Estamos muy emocionados de celebrar la llegada de nuestra pequeña
+            princesa. 💖
+          </Typography>
+
+          <Box mt={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mr: 2 }}
+              onClick={() => router.push("/admin")}
+            >
+              Administrar Invitaciones
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={() => alert("¡Gracias por tu visita!")}
+            >
+              Más Información
+            </Button>
+          </Box>
+        </Paper>
+      </Container>
+    </>
   );
 }
